3bbe080e688985d9027ddbfbd0929bf7eca49241
malaveaa <antoniomalavej@gmail.com>
2022-07-28 16:05:54 -0300

    i dont know

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -65,7 +65,6 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
- 
 }
 
 function tienenMismaLongitud(str1, str2) {
